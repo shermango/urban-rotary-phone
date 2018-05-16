9b3ff765c3e80f55b4b17199a37543891a7e5ff2
Sherman Chen <shermchen@paypal.com>
2018-05-15 21:16:44 -0700

    (chore) npm install morgan for logging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
