c68d1cd892d41db082eae8fd1515218339b7ad11
Sherman Chen <shermchen@paypal.com>
2018-05-29 23:57:13 -0700

    (wip) setting up redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,23 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const middleware = [thunk];
+
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 render(
   <Provider store={store}>
