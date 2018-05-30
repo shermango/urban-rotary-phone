ab485442ec9968c91ce092bc4f673eec93a8d0d2
Sherman Chen <shermchen@paypal.com>
2018-05-29 18:32:04 -0700

    (feat) setting up reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-render(<App />, document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
