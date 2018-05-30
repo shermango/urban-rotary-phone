86e048555715d359f6e1319854e61c5b01256e2f
Sherman Chen <shermchen@paypal.com>
2018-05-30 00:49:18 -0700

    (feat) wire reducer and render prop

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,12 +2,7 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
-  let data;
-  try {
-    data = await axios.get('/api/current_user');
+  const res = await axios.get('/api/current_user').catch(e => console.log(e));
 
-    return dispatch({ type: FETCH_USER, payload: data });
-  } catch (e) {
-    console.log(e);
-  }
+  return dispatch({ type: FETCH_USER, payload: res.data });
 };
