4de0905d905a9d33040bc98aa79024ff026614e9
Sherman Chen <shermchen@paypal.com>
2018-05-30 14:20:32 -0700

    (feat) POSTing to /api/stripe

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
 
   return dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+
+  return dispatch({ type: FETCH_USER, payload: res.data });
+};
