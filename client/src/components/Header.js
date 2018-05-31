7d9ff25991fc4d2e16e9bcbfa3abb2a0ea826409
Sherman Chen <shermchen@paypal.com>
2018-05-31 00:14:44 -0700

    (feat) add ui for credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -22,6 +22,9 @@ class Header extends Component {
             <li>
               <Payments />
             </li>
+            <li style={{ margin: '0 10px' }}>
+              Available Credits: {this.props.auth.credits}
+            </li>
             <li>
               <a href="/api/logout">Logout</a>
             </li>
